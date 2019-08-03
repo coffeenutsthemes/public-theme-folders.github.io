6c0e71e38253a415e66f8c25c947ec27a6a77a14
coffeenutsboutique <coffeenutsthemes@users.noreply.github.com>
2019-08-04 02:55:31 +0800

    Update custom404.js

--- javascripts/custom404.js
+++ javascripts/custom404.js
@@ -4,7 +4,7 @@ function init() {
 var pageTitle = $('.title').text();
 if (pageTitle == "Not Found"){
     $('.posts > .title').replaceWith( "<center><div class='title'>404</div></center>" );
-    $('.posts > p').replaceWith( "<center><p>Sorry, the content you are looking for could not be traced.</p><p style='font-size:8px!important;'><a href='/'>Return back to homepage</a></p></center>" );
+    $('.posts > p').replaceWith( "<p align='center'>Sorry, the content you are looking for could not be traced.</p><p align='center' style='font-size:8px!important;'><a href='/'>Return back to homepage</a></p>" );
 }}
 
 $(function($) {var allAccordions = $('.accordion div.data');var allAccordionItems = $('.accordion .accordion-item');$('.accordion > .accordion-item').click(function() {if($(this).hasClass('open')){$(this).removeClass('open');$(this).next().slideUp('500');}else{allAccordions.slideUp('500');allAccordionItems.removeClass('open');$(this).addClass('open');$(this).next().slideDown('500');return false;}});});
