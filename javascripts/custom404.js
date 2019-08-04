80a38fe3ca9931505f72e9d70a8f4095385ebdf7
coffeenutsboutique <coffeenutsthemes@users.noreply.github.com>
2019-08-04 14:15:51 +0800

    Update custom404.js

--- javascripts/custom404.js
+++ javascripts/custom404.js
@@ -3,8 +3,8 @@ $(init);
 function init() {
 var pageTitle = $('.title').text();
 if (pageTitle == "Not Found"){
-    $('.post .text > .title').replaceWith( "<div class='title'>404</div>" );
-    $('.post .text > p').replaceWith( "<p align='center'>Sorry, the content you are looking for could not be traced.</p><p align='center' style='font-size:8px!important;'><a href='/'>Return back to homepage</a></p>" );
+    $('.posts .post .text > .title').replaceWith( "<div class='title'>404</div>" );
+    $('.posts .post .text > p').replaceWith( "<p align='center'>Sorry, the content you are looking for could not be traced.</p><p align='center' style='font-size:8px!important;'><a href='/'>Return back to homepage</a></p>" );
 }}
 
 $(function($) {var allAccordions = $('.accordion div.data');var allAccordionItems = $('.accordion .accordion-item');$('.accordion > .accordion-item').click(function() {if($(this).hasClass('open')){$(this).removeClass('open');$(this).next().slideUp('500');}else{allAccordions.slideUp('500');allAccordionItems.removeClass('open');$(this).addClass('open');$(this).next().slideDown('500');return false;}});});
